0e81f445cd8e082209a2ce566b42908fb3b1afd7
S <stefan030699@yahoo.com>
2025-05-21 20:02:50 +0300

    added id's for the products

--- src/main/java/com/example/Store/DataLoader/DataLoader.java
+++ src/main/java/com/example/Store/DataLoader/DataLoader.java
@@ -16,8 +16,8 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        productRepository.save(new Product(null, "The Legend of Zelda", "Action-adventure game by Nintendo", 59.99, 10));
-        productRepository.save(new Product(null, "Minecraft", "Sandbox building game", 26.95, 25));
-        productRepository.save(new Product(null, "Cyberpunk 2077", "Open world RPG by CD Projekt", 49.99, 7));
+        productRepository.save(new Product(1L, "The Legend of Zelda", "Action-adventure game by Nintendo", 59.99, 10));
+        productRepository.save(new Product(2L, "Minecraft", "Sandbox building game", 26.95, 25));
+        productRepository.save(new Product(3L, "Cyberpunk 2077", "Open world RPG by CD Projekt", 49.99, 7));
     }
 }
