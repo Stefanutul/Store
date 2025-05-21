862014f53f4100ecc26b3ec15ca1173f2547312d
S <stefan030699@yahoo.com>
2025-05-21 19:39:09 +0300

    DTO  & Mapper classes  added

---
+++ src/test/java/Mapper/ProductMapper.java
@@ -0,0 +1,27 @@
+package Mapper;
+
+
+import com.example.Store.DTO.ProductRequestDTO;
+import com.example.Store.DTO.ProductResponseDTO;
+import com.example.Store.Models.Product;
+
+public class ProductMapper {
+
+    public static Product toEntity(ProductRequestDTO dto) {
+        return Product.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .price(dto.getPrice())
+                .quantity(dto.getQuantity())
+                .build();
+    }
+
+    public static ProductResponseDTO toDTO(Product product) {
+        return ProductResponseDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .quantity(product.getQuantity())
+                .build();
+    }
+}
---
+++ src/test/java/com/example/Store/DTO/ProductRequestDTO.java
@@ -0,0 +1,31 @@
+package com.example.Store.DTO;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequestDTO {
+
+    @NotBlank(message = "Product name must not be blank")
+    private String name;
+
+    @Size(max = 255, message = "Description can't exceed 255 characters")
+    private String description;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    @Min(value = 0, message = "Quantity cannot be negative")
+    private int quantity;
+}
