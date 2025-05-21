f7c15e160c32601ffa9844437d5de534bab48032
S <stefan030699@yahoo.com>
2025-05-21 19:28:09 +0300

    Product class initialized

---
+++ src/test/java/com/example/Store/Models/Product.java
@@ -0,0 +1,26 @@
+package com.example.Store.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private int quantity;
+}
