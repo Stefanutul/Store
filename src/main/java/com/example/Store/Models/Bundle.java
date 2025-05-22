24f848c353542a4a365bf02e3076b18f0fcae237
S <stefan030699@yahoo.com>
2025-05-22 19:42:06 +0300

    Bundle class / wip / future feature to be added

---
+++ src/main/java/com/example/Store/Models/Bundle.java
@@ -0,0 +1,54 @@
+package com.example.Store.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "bundle")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Bundle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+    private Double bundlePrice;
+
+    private LocalDateTime dateCreated;
+    private LocalDateTime lastUpdated;
+
+    private LocalDateTime expiryDate;
+
+    @ManyToMany
+    @JoinTable(
+            name = "bundle_products",
+            joinColumns = @JoinColumn(name = "bundle_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    @PrePersist
+    public void prePersist() {
+        dateCreated = LocalDateTime.now();
+        lastUpdated = LocalDateTime.now();
+        expiryDate = dateCreated.plusWeeks(1);
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        lastUpdated = LocalDateTime.now();
+    }
+}
